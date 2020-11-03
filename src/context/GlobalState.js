567c49250ee08a883804abb90349d9e7c54dfba3
robertoandres24 <robertoandres24@gmail.com>
2020-11-03 00:05:25 -0300

    watchlist: fetch movies from global context. and remove a movie form watchlist functionality

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -22,10 +22,16 @@ export const GlobalProvider = props => {
   const addMovieToWatchlist = (movie) => {
     dispatch({ type: 'ADD_MOVIE_TO_WATCHLIST', payload: movie })
   }
+  const removeMovieFromWatchlist = (id) => {
+    dispatch({ type: 'REMOVE_MOVIE_FROM_WATCHLIST', payload: id })
+  }
 
 
   return (
-    <GlobalContext.Provider value={{ watchlist: state.watchlist, watched: state.watched, addMovieToWatchlist }}>
+    <GlobalContext.Provider value={{
+      watchlist: state.watchlist, watched: state.watched,
+      addMovieToWatchlist, removeMovieFromWatchlist
+    }}>
       {props.children}
     </GlobalContext.Provider>
   )
